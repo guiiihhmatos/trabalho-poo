6ec50220a04971a60f5bf1c0dc8d50cd2ae97bb6
Guilherme Santana <gui.santana.matos@gmail.com>
2023-05-29 19:28:48 -0300

    IMPROVEMENT - models livro e user criadas, applisterner criado, api criada e conectado com o sqlite

--- livrariaPoo/src/java/model/livro.java
+++ livrariaPoo/src/java/model/livro.java
@@ -5,24 +5,167 @@
 package model;
 
 import java.util.Date;
+import java.util.ArrayList;
+import java.sql.*;
+import web.AppListener;
 
-public class livro {
+public class Livro {
     
-    private long id;
+    private long rowId;
     private String titulo;
     private String autor;
     private String editora;
-    private Date ano_publicacao;
+    private String ano_publicacao;
     private String isbn;
     private String descricao;
-    private boolean disponibilidade;
+    private String disponibilidade;
 
-    public long getId() {
-        return id;
+    public Livro(long rowId, String titulo, String autor, String editora, String ano_publicacao, String isbn, String descricao, String disponibilidade) {
+        this.rowId = rowId;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.ano_publicacao = ano_publicacao;
+        this.isbn = isbn;
+        this.descricao = descricao;
+        this.disponibilidade = disponibilidade;
+    }
+    
+    public static String getCreateStatement(){
+        return "CREATE TABLE IF NOT EXISTS livro("
+                +"titulo VARCHAR(50) NOT NULL,"
+                +"autor VARCHAR(100) NOT NULL,"
+                +"editora VARCHAR(40) NOT NULL,"
+                +"ano_publicacao datetime NOT NULL,"
+                +"isbn VARCHAR(13) UNIQUE NOT NULL,"
+                +"descricao VARCHAR(150) NOT NULL,"
+                +"disponibilidade VARCHAR(40) NOT NULL"
+                +");";      
+    }
+    
+     public static ArrayList<Livro> getLivros() throws Exception{
+        
+        ArrayList<Livro> list = new ArrayList<>();
+        Connection conexao = AppListener.getConnection();
+        Statement stmt = conexao.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT rowid, * from users");
+        
+        while(rs.next())
+        {
+            long rowId = rs.getLong("rowid");
+            String titulo = rs.getString("titulo");
+            String autor = rs.getString("autor");
+            String editora = rs.getString("editora");
+            String ano_publicacao = rs.getString("ano_publicacao");
+            String isbn = rs.getString("isbn");
+            String descricao = rs.getString("descricao");
+            String disponibilidade = rs.getString("disponibilidade");
+            
+            
+            list.add(new Livro(rowId, titulo, autor, editora, ano_publicacao, isbn, descricao, disponibilidade));
+        }
+        
+        rs.close();
+        stmt.close();
+        conexao.close();
+        
+        return list;
+    }
+    
+    public static Livro getLivro(String isbn) throws Exception{
+        
+        Livro livro = null;
+        Connection conexao = AppListener.getConnection();
+        String sql = "SELECT rowid, * from livro WHERE isbn=?";
+        
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, isbn);
+        ResultSet rs = stmt.executeQuery();
+        
+        if(rs.next())
+        {
+            long rowId = rs.getLong("rowid");
+            String titulo = rs.getString("titulo");
+            String autor = rs.getString("autor");
+            String editora = rs.getString("editora");
+            String ano_publicacao = rs.getString("ano_publicacao");
+            String descricao = rs.getString("descricao");
+            String disponibilidade = rs.getString("disponibilidade");
+            
+            livro = new Livro(rowId, titulo, autor, editora, ano_publicacao, isbn, descricao, disponibilidade);
+            
+            
+        }
+        
+        rs.close();
+        stmt.close();
+        conexao.close();
+        
+        return livro;
+    }
+    
+    public static void insertLivro(String titulo, String autor, String editora, String ano_publicacao, String isbn, String descricao, String disponibilidade) throws Exception{
+        
+        Connection conexao = AppListener.getConnection();
+        String sql = "INSERT INTO users(titulo, autor, editora, ano_publicacao, isbn, descricao, disponibilidade)"
+                + "VALUES (?,?,?,?,?,?,?)";
+        
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        
+        stmt.setString(1, titulo);
+        stmt.setString(2, autor);
+        stmt.setString(3, editora);
+        stmt.setString(4, ano_publicacao);
+        stmt.setString(5, isbn);
+        stmt.setString(6, descricao);
+        stmt.setString(7, disponibilidade);
+        
+        stmt.execute();
+        conexao.close();
+        stmt.close();
+        
+    }
+    
+    public static void updateLivro(String titulo, String autor, String editora, String ano_publicacao, String isbn, String descricao, String disponibilidade) throws Exception{
+        
+        Connection conexao = AppListener.getConnection();
+        String sql = "UPDATE users SET titulo, autor, editora, ano_publicacao, isbn, descricao, disponibilidade WHERE isbn=?";
+        
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        
+        stmt.setString(1, titulo);
+        stmt.setString(2, autor);
+        stmt.setString(3, editora);
+        stmt.setString(4, ano_publicacao);
+        stmt.setString(5, descricao);
+        stmt.setString(6, disponibilidade);
+        stmt.setString(7, isbn);
+        
+        stmt.execute();
+        conexao.close();
+        stmt.close();
+        
+    }
+    
+    public static void deleteLivro(long rowId) throws Exception{
+        
+        Connection conexao = AppListener.getConnection();
+        String sql = "DELETE FROM livro WHERE rowid=?";
+        
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        
+        stmt.setLong(1, rowId);
+        stmt.execute();
+        conexao.close();
+        stmt.close();
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public long getId() {
+        return rowId;
+    }
+
+    public void setId(long rowId) {
+        this.rowId = rowId;
     }
 
     public String getTitulo() {
@@ -49,11 +192,11 @@ public class livro {
         this.editora = editora;
     }
 
-    public Date getAno_publicacao() {
+    public String getAno_publicacao() {
         return ano_publicacao;
     }
 
-    public void setAno_publicacao(Date ano_publicacao) {
+    public void setAno_publicacao(String ano_publicacao) {
         this.ano_publicacao = ano_publicacao;
     }
 
@@ -73,11 +216,11 @@ public class livro {
         this.descricao = descricao;
     }
 
-    public boolean isDisponibilidade() {
+    public String isDisponibilidade() {
         return disponibilidade;
     }
 
-    public void setDisponibilidade(boolean disponibilidade) {
+    public void setDisponibilidade(String disponibilidade) {
         this.disponibilidade = disponibilidade;
     }
     
